67c7fbb02d09ed0bc32b8aec1902a41c288d7fc3
DeepPytorch <cohenalexanderphillips@gmail.com>
2026-04-21 18:57:55 -0500

    Create Cancer_Model.py

---
+++ Cancer_Model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+class CancerModel(nn.Module):
+    def __init__(self):
+        super(CancerModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 1)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))
+        return x
+
